0b0403f2fd7ba9bf3178587e9787a1451263d60b
Ellie Hensley <ehensley001@student.butte.edu>
2016-08-25 20:52:37 +0000

    Updated Lab1 for Ascii program.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,48 @@
+// Lab 1: Ascii Art
+// This program outputs ascii art of Eevee, my favorite pokemon!
+// Ellie Hensley
+// 8-25-16
+// Ascii Eevee courtesy of: http://www.fiikus.net/?pokedex
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "                                      |" << endl;
+    cout << "                                     /|" << endl;
+    cout << "                                   ,\' |" << endl;
+    cout << "                                  .   |" << endl;
+    cout << "                                    | |" << endl;
+    cout << "                                 \' \'| |" << endl;
+    cout << "                                / / | |" << endl;
+    cout << "       _,.-\"\"--._              / /  | |" << endl;
+    cout << "     ,'          `.           j \'   \' \'" << endl;
+    cout << "   ,'              `.         ||   / ,                         ___..--," << endl;
+    cout << "  /                  \\        ' `.'`.-.,-\".  .       _..---\"\"'' __, ,'" << endl;
+    cout << " /                    \\        \\\` .\"\`      `\"\'\\   ,\'\"_..--\'\'\"\"\"\'.\'.\'" << endl;
+    cout << ".                      .      .\'-\'             \\,\' ,\'         ,\',\'" << endl;
+    cout << "|                      |      ,`               \' .`         .\' /" << endl;
+    cout << "|                      |     /          ,\"`.  \' `-. _____.-\' .\'" << endl;
+    cout << "\'                      |..---.|,\".      | | .  .-\'\"\"   __.,-\'" << endl;
+    cout << " .                   ,'       ||,|      |.\' |    |\"\"`\'\"" << endl;
+    cout << "  `-._   `._.._____  |        || |      `._,\'    |" << endl;
+    cout << "      `.   .       `\".     ,\'\"| \"  `\'           ,+." << endl;
+    cout << "        \\  \'         |    \'   |   .....        .\'  `." << endl;
+    cout << "         .\'          \'     7  \".              ,\'     \\" << endl;
+    cout << "                   ,\'      |    `..        _,\'      F" << endl;
+    cout << "                  .        |,      `\'----\'\'         |" << endl;
+    cout << "                  |      ,\"j  /                   | \'" << endl;
+    cout << "                  `     |  | .                 | `,\'" << endl;
+    cout << "                   .    |  `.|                 |/" << endl;
+    cout << "                    `-..\'   ,\'                .\'" << endl;
+    cout << "                            | \\             ,\'\'" << endl;
+    cout << "                            |  `,\'.      _,\' /" << endl;
+    cout << "                            |    | ^.  .\'   /" << endl;
+    cout << "                             `-\'.\' j` V    /" << endl;
+    cout << "                                   |      /" << endl;
+    cout << "                                   |     /" << endl;
+    cout << "                                   |   ,\'" << endl;
+    cout << "                                    `\"\"" << endl;
+    cout << "" << endl;
+}
