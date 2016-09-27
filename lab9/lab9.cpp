bc11792580fe3f7af9908c9b15a419533d1b0603
Ellie Hensley <ehensley001@student.butte.edu>
2016-09-27 01:00:23 +0000

    Lab9 Submission (I promise I can follow directions)

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -97,26 +97,3 @@ string TemperatureConverter::PrintTemperatures()
 {
     cout << kelvin_ << " K is the same as " << GetTempAsFahrenheit() << " F and " <<  GetTempAsCelsius() << " C." << endl << endl;
 }
-
-int main ()
-{
-    TemperatureConverter temp1; //testing default constructor
-    TemperatureConverter temp2(274); //testing overloaded constructor
-    
-    temp1.PrintTemperatures();
-    temp2.PrintTemperatures();
-    
-    temp1.SetTempFromKelvin(400.15); //testing mutator function
-    cout << temp1.GetTempFromKelvin() << endl; //testing accessor function
-    temp1.PrintTemperatures();
-    
-    temp2.SetTempFromCelsius(32); //testing other functions
-    cout << temp2.GetTempAsCelsius() << endl;
-    temp2.PrintTemperatures();
-    
-    temp2.SetTempFromFahrenheit(32);
-    cout << temp2.GetTempAsFahrenheit() << endl;
-    temp2.PrintTemperatures();
-    
-    return 0;
-}
