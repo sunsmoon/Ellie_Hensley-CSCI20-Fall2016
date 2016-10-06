4134dcd06cc2f81a54e138f8bda5d50a5980e66a
Ellie Hensley <ehensley001@student.butte.edu>
2016-10-06 20:03:37 +0000

    changes to lab 12 (I promise I can read)

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -18,48 +18,40 @@ int main(){
     
     
     switch (response_1) {
-        case 'Y':                               // If the user likes the outdoors
-        case 'y':                               // Ask if they like to run & store their response.
+        case 'Y':                                                   // If the user likes the outdoors
+        case 'y':                                                   // Ask if they like to run & store their response.
             cout << "Do you like to run? Y/N ";
             cin >> response_2;
             
             switch (response_2) {
-                case 'Y':                       // If the user likes to run
-                case 'y':                       // tell them they are a horse
+                case 'Y':                                           // If the user likes to run
+                case 'y':                                           // tell them they are a horse
                     cout << "You are a horse!";
                     break;
 
-                default:                        // Otherwise (if they hate running), they are a turtle.
+                default:                                            // Otherwise (if they hate running), they are a turtle.
                     cout << "You are a turtle!";
                     break;
             }
             break;
         
-        default:                                // User hates the outdoors. Do they like cheese? (Ask & store)
+        default:                                                    // User hates the outdoors. Ask another question!
             cout << "Do you like cheese? Y/N ";
             cin >> response_2;
             
-            switch (response_2) {
-                case 'Y':                       // If the user likes cheese
-                case 'y':
-                    cout << "You are a mouse!";
-                    break;
-                
-                default:
-                    cout << "Do you like to swim? Y/N ";    // User doesn't like cheese, ask another question.
-                    cin >> response_3;
-                    
-                    switch (response_3) {
-                        case 'Y':                   // User likes to swim
-                        case 'y':                   
-                            cout << "You are a fish!";
-                            break;
-                
-                        default:                    // User hates swimming.
-                            cout << "You are a cat!";
-                            break;
-                    }
-                    break;
+            if (response_2 == 'Y' || response_2 == 'y') {           // User likes cheese
+                cout << "You are a mouse!";
+            }
+            else
+                cout << "Do you like to swim? Y/N ";                // User doesn't like cheese, ask another question.
+                cin >> response_3;
+            
+                if (response_3 == 'Y' || response_3 == 'y') {       // User likes to swim
+                    cout << "You are a fish!";
+                }   
+                else {                                              // User doesn't like to swim.
+                    cout << "You are a cat!";
+                }
             }
             break;
     }
